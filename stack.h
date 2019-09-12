0cfe2a7b40a0418a619bdc24451e0c3c14ecfd3a
Alexey Akhundov <38561919+TheSeems@users.noreply.github.com>
2019-09-12 14:17:41 +0300

    Stack implementation

---
+++ stack.h
@@ -0,0 +1,62 @@
+#include <iostream>
+#ifndef _CALCSTACK_H
+#define _CALCSTACK_H
+
+template <typename T>
+class stack {
+  struct node {
+    T elem;
+    node * next {nullptr};
+    explicit node (const T& elem): elem(elem) {};
+    node() = default;
+  } * head;
+
+  size_t sz;
+
+public:
+  stack() {
+    head = new node();
+  }
+
+  void push(const T& t) {
+    node * nw = new node(t);
+    nw->next = head->next;
+    head->next = nw;
+
+    ++sz;
+  }
+
+  bool empty() {
+    return head->next == nullptr;
+  }
+
+  T top() {
+    assert(!empty());
+    return head->next->elem;
+  }
+
+  size_t size() {
+    return sz;
+  }
+
+  void pop () {
+    if (!empty()) {
+      node * nw = head->next->next;
+      delete head->next;
+      head->next = nw;
+      
+      --sz;
+    }
+  }
+
+  void clear() {
+    while(!empty())
+      pop();
+  }
+
+  ~stack() {
+    clear();
+  }
+};
+
+#endif // stack.h
