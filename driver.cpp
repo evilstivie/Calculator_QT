d45f929eae5f2d10b669a04b51f5833bd4ac0303
Alexey Akhundov <38561919+TheSeems@users.noreply.github.com>
2019-09-13 21:48:43 +0300

    Making driver calculate

--- driver.cpp
+++ driver.cpp
@@ -1,9 +1,15 @@
 #include "algo.h"
+#include "smath.h"
 #include <iostream>
+#include <string>
+#include <iomanip>
 
 int main () {
-  std::string inp;
-  std::getline(std::cin, inp);
-  std::cout << calc_polish(inp) << std::endl;
+  std::string s;
+  std::getline(std::cin, s);
+  std::string polish;
+  to_polish(s, polish);
+  std::cout << polish << std::endl;
+  std::cout << "Eval = " << std::fixed << std::setprecision(15) << calc_polish(polish) << std::endl;
   return 0;
 }
