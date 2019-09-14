5f73c866f5e2a0939dee3000670e2da28f12eb6c
Alexey Akhundov <38561919+TheSeems@users.noreply.github.com>
2019-09-14 13:33:27 +0300

    Support for unary '-' and '+'

--- driver.cpp
+++ driver.cpp
@@ -7,9 +7,30 @@
 int main () {
   std::string s;
   std::getline(std::cin, s);
+
+  std::string lol;
+  for (int i = 0; i < s.size(); i++) {
+    char c = s[i];
+    bool prev = i == 0 ? false : (isdigit(s[i - 1]) || s[i - 1] == ')');
+    bool next = i == s.size() - 1 ? false : (isdigit(s[i + 1]) || s[i + 1] == ')');
+    if (!prev && next && (c == '+' || c == '-')) {
+      ++i;
+      double d = read_double(s, i);
+      --i;
+      if (c == '+')
+        lol += std::to_string(d) + "+0";
+      if (c == '-')
+        lol += "0-" + std::to_string(d);
+    } else 
+      lol += c;
+  }
+
+  std::cout << "Pre-processed string = " << lol << std::endl;
+
   std::string polish;
-  to_polish(s, polish);
-  std::cout << polish << std::endl;
-  std::cout << "Eval = " << std::fixed << std::setprecision(15) << calc_polish(polish) << std::endl;
+  to_polish(lol, polish);
+  
+  std::cout << "Polish notation = " << polish << std::endl;
+  std::cout << "Eval = " << std::fixed << std::setprecision(2) << calc_polish(polish) << std::endl;
   return 0;
 }
