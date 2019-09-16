eb954ae41883dad38706582dc16be159c67917e1
Alexey Akhundov <38561919+TheSeems@users.noreply.github.com>
2019-09-16 16:45:05 +0300

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -1,6 +1,6 @@
 #include "algo.h"
 #include "hashmap.h"
-#include "smath.h"
+#include <cmath>
 #include <iostream>
 #include <string>
 #include <iomanip>
@@ -9,11 +9,26 @@
 void test_main() {
   std::string s;
   std::getline(std::cin, s);
+
+  std::cout <<  std::fixed << std::setprecision(4);
   
   HashMap<std::string, double, string_hash> vars;
   vars.put("e", EULER);
   vars.put("pi", PI);
-  vars.put("x", 1.0);
+
+  std::string inp;
+  while (std::cin >> inp) {
+    std::cout << "name " << inp << std::endl;
+    if (inp == "exit")
+      break;
+    std::string value;
+    std::cin >> value;
+    std::cout << " value to parse " << value << std::endl;
+    int _ = 0;
+    double d = read_double(value, _);
+    vars.put(inp, d); 
+    std::cout << "got it: " << inp << " => " << d << std::endl;
+  }
 
   trim_unary(s, vars);  // replace unary plus and minus with 0 +- expr
 
@@ -23,16 +38,7 @@ void test_main() {
   to_polish(s, polish, vars);
   
   std::cout << "Polish notation = " << polish << std::endl;
-  std::cout << "Result = " << std::fixed << std::setprecision(2) << calc_polish(polish) << std::endl;
-}
-
-void test_hashmap() {
-  HashMap<std::string, double, string_hash> map;
-  map.put("e", EULER);
-  map.put("pi", PI);
-
-  std::cout << map.get("e") << std::endl;
-  std::cout << map.get("pi") << std::endl;
+  std::cout << "Result = " << calc_polish(polish) << std::endl;
 }
 
 int main () {
